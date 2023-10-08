fdc1f39d84d31a68f64938b70673501ad00089b5
Ahmed <AhmedITfixer@gmail.com>
2023-10-08 15:13:08 +0100

    Second Commit

---
+++ loop and arrays.js
@@ -0,0 +1,20 @@
+// for(let i =0;i < 20;i++){
+//     console.log("Ahmed")
+// }
+
+// let count = 0
+// while(count < 10){
+//     console.log("Mohamed")
+//     count++
+// }
+
+let c = 0;
+do {
+    console.log("I am in the loop body")
+    c++
+} while(false)
+
+
+// Write an array that hold 10 names of your friends and family
+
+let friendsAndFamily = ["Ahmed", "Ali", "Mohamed", "Hamza", "Ayuub", "Anas", "Farah"]
