616704e75a470de93bb1f3ba6fd989ac746bd3db
Ahmed <AhmedITfixer@gmail.com>
2023-10-08 15:43:12 +0100

    Second Commit

--- loop and arrays.js
+++ loop and arrays.js
@@ -8,13 +8,34 @@
 //     count++
 // }
 
-let c = 0;
-do {
-    console.log("I am in the loop body")
-    c++
-} while(false)
+// let c = 0;
+// do {
+//     console.log("I am in the loop body")
+//     c++
+// } while(false)
 
 
 // Write an array that hold 10 names of your friends and family
 
 let friendsAndFamily = ["Ahmed", "Ali", "Mohamed", "Hamza", "Ayuub", "Anas", "Farah"]
+
+// How many interation functions that come with arrays that you know
+
+// friendsAndFamily.forEach()
+// friendsAndFamily.map()
+// friendsAndFamily.filter()
+// friendsAndFamily.reduce()
+// friendsAndFamily.every()
+
+// print every element in the friends array and concat and put hello infront of the name
+
+
+// friendsAndFamily.forEach{
+//     function myName(name){
+//         console.log("Hello " + name)
+//     }
+// }
+
+friendsAndFamily.forEach; { name => console.log("Hello " + name)}
+
+
